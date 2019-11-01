33afc9381863df8fedcd49e59c0bbf1d18651876
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-01 14:59:01 +0600

    Model Added

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=2083)
