5e712e3b817546fbac0a5092f148429c7150afb9
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-02 00:19:00 +0600

    Product Details Update With Admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Product
-# Register your models here.
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Product, ProductAdmin)
