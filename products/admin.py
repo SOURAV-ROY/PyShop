399c857f8cf79ae26e8a016f30e5ca5df37513fe
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-02 00:12:08 +0600

    Database Update

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products
+from .models import Product
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
 
+
 class Offer(models.Model):
     code = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
     discount = models.FloatField()
-
