d033e041386a6f5b8a92c4f8cc835b2a9e29401a
SOURAV ROY <souravroy.info13@gmail.com>
2019-11-02 13:47:45 +0600

    Index fun Update With templates

--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 
 # /products -> index
 # Uniform Resource Locator (Address)
 
 def index(request):
+    products = Product.objects.all()
     return HttpResponse("Hello World")
 
 
